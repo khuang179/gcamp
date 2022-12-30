6dbbc755017c4b0e2cb5c292dcb4828927e24ef3
khuang179 <112203948+khuang179@users.noreply.github.com>
2022-12-30 17:54:40 -0600

    Update GCAMP_activity_analysis.m

--- GCAMP_activity_analysis.m
+++ GCAMP_activity_analysis.m
@@ -12,48 +12,60 @@ disp(filename);
 raw_data=csvread(filename);
 %Ask for time interval
 prompt="Please enter time interval of the image sequence in sec";
-ans=input(prompt)
-Time=(raw_data(2:end,1).*ans)-ans
+tint=input(prompt)
+Time=(raw_data(2:end,1).*tint)-tint
 
 %trim the raw data.
 Trimmed_data=raw_data(2:end, 2:end);
 background=Trimmed_data(:,end);
 init=Trimmed_data-background;
 %Analysis
-pkg load signal;
-
-prompt2="Choose column for analysis";
-n=input(prompt2);
-r=medfilt1((init(1:end,n)), 200)
-a=(init(1:end,n))./r
-base=abs(a-1)
-[val t]=findpeaks(base,"MinPeakHeight",1)
+pkg load signal
+  n=input("Choose column for analysis");
+  r=medfilt1((init(1:end,n)), 200)
+  a=(init(1:end,n))./r
+  [val t]=findpeaks(a,"MinPeakHeight",1.5,"MinPeakDistance",4)
+  t=t*tint
 
 %Result plot for individual column
 figure(1);
-plot(Time,base,'m')
+plot(Time,a,'b')
 hold on;
-scatter(t, val);
+scatter(t, val, 'm');
 xlabel('Time (sec)');
 ylabel('Amplitude(\Delta F/F_{0}');
-
+ylim([0 5])
+yticks(0:0.5:5)
+saveas(figure(1),"gCaMP activity normalized.png");
 
 figure(2);
-plot(init(1:end,n),'g')
+plot(Time,init(1:end,n),'g');
 hold on;
-plot(r);
+plot(Time,r);
 xlabel('Time (sec)');
 ylabel('fluorescnece intensity');
+saveas(figure(2),"Nonnormalized activity with filtered curve.png");
 
 
-
-
-
-
-
-
-
-
-
-
-%[r,c]=size(init)
+if isfile('whole sample activity imagesc plot.png')
+  button=questdlg('A matrix plot for all samples of this specimen has already been created. Do you still want to proceed and make a new one to overwrite the old one?','Warning!','Yes','No','No');
+  if strcmp(button,'Yes')
+    figure(3)
+    activity=medfilt1(init, 200)
+    activitysample=init(:,1:end-1)
+    activity_normalized=activitysample./(activity(:,1:end-1))
+    activity_normalized=activity_normalized'
+    imagesc(activity_normalized)
+    colorbar()
+    saveas(figure(3),"whole sample activity imagesc plot.png")
+  elseif strcmp(button,'No'),return,end
+else
+    figure(3)
+    activity=medfilt1(init, 200)
+    activitysample=init(:,1:end-1)
+    activity_normalized=activitysample./(activity(:,1:end-1))
+    activity_normalized=activity_normalized'
+    imagesc(activity_normalized)
+    colorbar()
+    saveas(figure(3),"whole sample activity imagesc plot.png")
+  end
